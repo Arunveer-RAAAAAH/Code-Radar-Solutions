cf6966c306dba73668d91281a55139a2e1a173e6
Arunveer-RAAAAAH <arunveer0862.becse24@chitkara.edu.in>
2025-01-31 04:34:37 +0530

    Solution to Grade Based on Marks

---
+++ Grade Based on Marks@@6788dd63490637cfc44e1b96/code.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+char* welcome() {
+    return "Welcome to Code Radar!";
+}
+
+int main() {
+    int a;
+    scanf("%d",&a);
+
+    switch(a){
+        case a>=90:
+        printf('A');
+        case a>=80 && a<90:
+        printf('B');
+        case a>=70 && a<80:
+        printf('C');
+        case a>=60 && a<70:
+        printf('D');
+        case a<60:
+        printf('F');
+    }
+}
