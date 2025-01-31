574607facaeb16da38f129becee68f14e3a86e3c
Arunveer-RAAAAAH <arunveer0862.becse24@chitkara.edu.in>
2025-01-31 06:24:39 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -5,9 +5,9 @@ char* welcome() {
 }
 
 int main() {
-    float a,b;
+    int a,b;
     char c;
-    scanf("%d %d %c",&a,&b,&c);
+    scanf("%d%d%c",&a,&b,&c);
     switch(c){
         case '+':
             printf("%d",a+b);
